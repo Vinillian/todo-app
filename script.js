10c652257322949911ec77b4d817dd0636ebd0f9
Vinillian <ber07@inbox.ru>
2026-08-08 01:07:30 +0300

    Add event listener for add button with task addition logic

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ list.innerHTML = '<li>Первая задача</li><li>Вторая задач�
 list.innerHTML += '<li>Третья задача</li>';
 
 // Добавляем задачу в консоли (пустая строка)
-const taskInput = document.getElementById('taskInput');
-const text = taskInput.value;
-list.innerHTML += '<li>' + text + '</li>';
+// const taskInput = document.getElementById('taskInput');
+// const text = taskInput.value;
+// list.innerHTML += '<li>' + text + '</li>';
+
+
+// Добавляем взаимодействие с кнопкой
+
+const addBtn = document.getElementById('addBtn');
+
+addBtn.addEventListener('click', function () {
+    const text = taskInput.value;
+    list.innerHTML += '<li>' + text + '</li>';
+    taskInput.value = ''; // очищаем поле после добавления
+});
