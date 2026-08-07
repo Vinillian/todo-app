40319cfb91900d95f77d6b2f93ca5b29e36dfe1a
Vinillian <ber07@inbox.ru>
2026-08-08 01:05:36 +0300

    Add innerHTML manipulation and reading input value

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ console.log(list.innerHTML); // выведет: "<li>Купить молоко</
 list.innerHTML = '<li>Первая задача</li><li>Вторая задача</li>';
 list.innerHTML += '<li>Третья задача</li>';
 
+// Добавляем задачу в консоли (пустая строка)
 const taskInput = document.getElementById('taskInput');
 const text = taskInput.value;
 list.innerHTML += '<li>' + text + '</li>';
