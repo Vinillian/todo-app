df9c1a37e9860fbf82ed4ddd5100ae7a12b74691
Vinillian <ber07@inbox.ru>
2026-08-08 01:08:36 +0300

    Refactor: extract add task logic and handle Enter key

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
+
 const list = document.getElementById('taskList');
-console.log(list.innerHTML); // выведет: "<li>Купить молоко</li>"
+const addBtn = document.getElementById('addBtn');
+const taskInput = document.getElementById('taskInput');
+
+// Стартовые задачи
 list.innerHTML = '<li>Первая задача</li><li>Вторая задача</li>';
 list.innerHTML += '<li>Третья задача</li>';
 
-// Добавляем задачу в консоли (пустая строка)
-// const taskInput = document.getElementById('taskInput');
-// const text = taskInput.value;
-// list.innerHTML += '<li>' + text + '</li>';
 
 
 // Добавляем взаимодействие с кнопкой
 
-const addBtn = document.getElementById('addBtn');
 
+// Обработчик клика на кнопку
 addBtn.addEventListener('click', function () {
-    const text = taskInput.value;
+    const text = taskInput.value.trim(); //убираем пробелы в начале и в конце
+    if(text === '') {
+        alert('Введите текст задачи!');
+        return; // выходим из функции, ничего не добавляем
+    }
     list.innerHTML += '<li>' + text + '</li>';
     taskInput.value = ''; // очищаем поле после добавления
+});
+
+// Обработчик нажатия на Enter в поле ввода
+taskInput.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        const text = taskInput.value.trim();
+        if (text === '') {
+            alert('Введите текст задачи!');
+            return;
+        }
+        list.innerHTML += '<li>' + text + '</li>';
+        taskInput.value = '';
+    }
 });
